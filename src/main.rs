e38928bec8f5906c0323d1957ddc37b7d53bebc1
NAGATA Hiroaki <nagata@handlena.me>
2024-12-31 22:57:39 +0900

    feat: call usecase trial

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use app::usecase;
+use color_eyre::eyre::Result;
+
 mod app;
 mod domain;
 mod infra;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let worker_id = domain::value_object::worker_id::WorkerID::new(0)?;
+    let repo = infra::repository::id::ID::new(worker_id)?;
+    let uc = usecase::generate::Generate::new(repo);
+    let id = uc.run()?;
+    println!("{:?}", id);
+    Ok(())
 }
