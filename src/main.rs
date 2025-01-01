1534b4cb821ce1a7e19fce024be3b4155c00c5be
NAGATA Hiroaki <nagata@handlena.me>
2025-01-01 19:02:01 +0900

    feat: call infra::interface::cli from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,62 @@
-use app::usecase;
+use clap::Parser;
 use color_eyre::eyre::Result;
 
 mod app;
 mod domain;
 mod infra;
 
+#[derive(clap::Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Worker ID
+    #[arg(short, long)]
+    worker_id: u32,
+
+    /// Issue a ID
+    #[arg(long, default_value_t = false)]
+    issue: bool,
+
+    /// Number of IDs to issue
+    #[arg(long)]
+    issues: Option<usize>,
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let worker_id = domain::value_object::worker_id::WorkerID::new(0)?;
-    let repo = infra::repository::id::ID::new(worker_id)?;
-    let uc = usecase::generate::Generate::new(repo);
-    let id = uc.run()?;
-    println!("{:?}", id);
+
+    let args = Args::parse();
+    if args.issue {
+        run_cli_issue(args)?;
+        return Ok(());
+    }
+
+    if let Some(_) = args.issues {
+        run_cli_issue_some(args)?;
+        return Ok(());
+    }
+
+    Ok(())
+}
+
+fn run_cli_issue(args: Args) -> Result<()> {
+    let worker_id = domain::value_object::worker_id::WorkerID::new(args.worker_id)?;
+    let repository = infra::repository::id::ID::new(worker_id)?;
+
+    let id = infra::interface::cli::Cli::new(repository).issue()?;
+    println!("{}", u64::from(id));
+
+    Ok(())
+}
+
+fn run_cli_issue_some(args: Args) -> Result<()> {
+    let worker_id = domain::value_object::worker_id::WorkerID::new(args.worker_id)?;
+    let repository = infra::repository::id::ID::new(worker_id)?;
+    let num = args.issues.unwrap();
+
+    let ids = infra::interface::cli::Cli::new(repository).issue_some(num)?;
+    for id in ids {
+        println!("{}", u64::from(id));
+    }
+
     Ok(())
 }
