4eb68382eb571e2eee8eaacb78be9c3bbe3ddb08
NAGATA Hiroaki <nagata@handlena.me>
2025-01-07 16:50:16 +0900

    style: digit grouping
        warning: digits grouped inconsistently by underscores
         --> src/domain/constant.rs:5:35
          |
        5 | pub const TIMESTAMP_OFFSET: u64 = 1420070400_000;
          |                                   ^^^^^^^^^^^^^^ help: consider:
        `1_420_070_400_000`
          |
          = help: for further information visit
        https://rust-lang.github.io/rust-clippy/master/index.html#inconsistent_digit_grouping
          = note: `#[warn(clippy::inconsistent_digit_grouping)]` on by default

--- src/domain/constant.rs
+++ src/domain/constant.rs
@@ -2,7 +2,7 @@ pub const WORKER_ID_BITS: i32 = 10;
 pub const SEQUENCE_BITS: i32 = 12;
 
 // 2015-01-01T00:00:00Z in milliseconds
-pub const TIMESTAMP_OFFSET: u64 = 1420070400_000;
+pub const TIMESTAMP_OFFSET: u64 = 1_420_070_400_000;
 
 pub const TIMESTAMP_MIN: u64 = TIMESTAMP_OFFSET;
 
