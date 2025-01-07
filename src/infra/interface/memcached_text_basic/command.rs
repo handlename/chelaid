3b438c7a06ccf3e5b26dcd5b305a17c0ee851782
NAGATA Hiroaki <nagata@handlena.me>
2025-01-07 10:14:30 +0900

    doc: for Command::execute

--- src/infra/interface/memcached_text_basic/command.rs
+++ src/infra/interface/memcached_text_basic/command.rs
@@ -4,7 +4,10 @@ use color_eyre::eyre::Result;
 pub use get::Get;
 
 pub trait Command {
-    fn execute(&self) -> Result<String>;
+    /// Execute the command and return the result.
+    /// The result is a vector of strings.
+    /// Each string represents a line of the response.
+    fn execute(&self) -> Result<Vec<String>>;
 
     /// Convert the command to a string.
     /// format: "<command> <arg1> <arg2> ..."
