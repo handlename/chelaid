handlename/chelaid

commit c3f9aa611b5cb3b09abd59df21bcd4b51d987513

refactor: judge by downcast

src/infra/interface/memcached_text_basic/command.rs
@@ -5,7 +5,7 @@ use color_eyre::eyre::Result;
 pub use end::End;
 pub use get::Get;
 
-pub trait Command {
+pub trait Command: std::any::Any {
     /// Execute the command and return the result.
     /// The result is a vector of strings.
     /// Each string represents a line of the response.
@@ -17,4 +17,6 @@ pub trait Command {
     /// Convert the command to a string.
     /// format: "<command> <arg1> <arg2> ..."
     fn to_string(&self) -> String;
+
+    fn as_any(&self) -> &dyn std::any::Any;
 }

src/infra/interface/memcached_text_basic/command/end.rs
@@ -29,4 +29,8 @@ impl Command for End {
     fn to_string(&self) -> String {
         String::from(self.command_name.clone())
     }
+
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
 }

src/infra/interface/memcached_text_basic/command/get.rs
@@ -53,6 +53,10 @@ impl Command for Get {
             self.keys.join(" ")
         )
     }
+
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
 }
 
 #[cfg(test)]

src/infra/server/tcp.rs
@@ -107,9 +107,10 @@ where
             }
             Ok(_) => match parser.parse(&buf) {
                 Ok(command) => {
-                    if command.command_name()
-                        == infra::interface::memcached_text_basic::CommandName::End
-                    {
+                    if let Some(_) = command
+                        .as_any()
+                        .downcast_ref::<infra::interface::memcached_text_basic::command::End>(
+                    ) {
                         log::debug!("END command from {}", address);
                         break;
                     }

commit ebd5f55fa109c70a89125e78ecdfba1758bb1b7c

refactor: remove unused codes
caused by c3f9aa611b5cb3b09abd59df21bcd4b51d987513 and
64d5baaab5f81a85e338a5f90f2fbe7938c3c9f2

src/infra/interface/memcached_text_basic.rs
@@ -3,5 +3,4 @@ pub mod command_name;
 mod error;
 mod parser;
 
-pub use command_name::CommandName;
 pub use parser::Parser;

src/infra/interface/memcached_text_basic/command.rs
@@ -11,12 +11,5 @@ pub trait Command: std::any::Any {
     /// Each string represents a line of the response.
     fn execute(&self) -> Result<Vec<String>>;
 
-    /// Return the command name.
-    fn command_name(&self) -> super::command_name::CommandName;
-
-    /// Convert the command to a string.
-    /// format: "<command> <arg1> <arg2> ..."
-    fn to_string(&self) -> String;
-
     fn as_any(&self) -> &dyn std::any::Any;
 }

src/infra/interface/memcached_text_basic/command/end.rs
@@ -1,18 +1,12 @@
 use color_eyre::eyre::Result;
 
-use crate::infra::interface::memcached_text_basic::command_name::CommandName;
-
 use super::Command;
 
-pub struct End {
-    command_name: CommandName,
-}
+pub struct End {}
 
 impl End {
     pub fn new() -> Self {
-        Self {
-            command_name: CommandName::End,
-        }
+        Self {}
     }
 }
 
@@ -20,14 +14,6 @@ impl Command for End {
     fn execute(&self) -> Result<Vec<String>> {
         // do nothing
         Ok(Vec::new())
-    }
-
-    fn command_name(&self) -> CommandName {
-        self.command_name.clone()
-    }
-
-    fn to_string(&self) -> String {
-        String::from(self.command_name.clone())
     }
 
     fn as_any(&self) -> &dyn std::any::Any {

src/infra/interface/memcached_text_basic/command/get.rs
@@ -5,8 +5,7 @@ use super::{super::command_name::CommandName, Command};
 use crate::{app, domain};
 
 pub struct Get {
-    command_name: CommandName,
-    keys: Vec<String>,
+    pub keys: Vec<String>,
     usecase: app::usecase::generate::Generate,
 }
 
@@ -24,7 +23,6 @@ impl Get {
         }
 
         Ok(Self {
-            command_name: CommandName::Get,
             keys,
             usecase: app::usecase::generate::Generate::new(repository),
         })
@@ -40,18 +38,6 @@ impl Command for Get {
         }
 
         Ok(results)
-    }
-
-    fn command_name(&self) -> CommandName {
-        self.command_name.clone()
-    }
-
-    fn to_string(&self) -> String {
-        format!(
-            "{} {}",
-            String::from(self.command_name.clone()),
-            self.keys.join(" ")
-        )
     }
 
     fn as_any(&self) -> &dyn std::any::Any {